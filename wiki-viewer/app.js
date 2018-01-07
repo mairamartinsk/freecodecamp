771d70647dbace2138b52f8fc099ab1f60fd81d0
Maira Martins K <mairamartinsk@gmail.com>
2018-01-07 23:51:04 +0100

    Initial commit, complete project

---
+++ wiki-viewer/app.js
@@ -0,0 +1,28 @@
+// API ENDPOINT
+// https://en.wikipedia.org/w/api.php?format=json&action=query&list=search&srsearch=Albert%20Einstein
+
+function getWikiResults() {
+  var searchTerm = $('input[type="text"]').val();
+  $.ajax({
+    type: 'GET',
+    url: 'https://en.wikipedia.org/w/api.php?origin=*&format=json&action=query&list=search&srsearch=' + searchTerm,
+    error: function(err) {
+      console.log(err);
+    },
+    success: function(res) {
+      var $retrieveResult = res.query.search;
+      $('ul').html('');
+      $retrieveResult.forEach(function(eachResult){
+        $('ul').append('<li><a href="https://en.wikipedia.org/?curid=' + eachResult.pageid +'" target="_blank"><h3>' + eachResult.title + '</h3><p>' + eachResult.snippet + '</p></a></li>')
+      });
+    }
+  });
+}
+
+$('input[type="text"]').on('keypress', function(event) {
+  // if user presses Enter (keycode 13), get input value
+  if (event.which === 13) {
+    getWikiResults();
+    $(this).val('');
+  }
+});
